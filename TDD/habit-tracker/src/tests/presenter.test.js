fd14f0b45c18a03681f713131910e8705f91ce2b
Benny <vvsogi@gmail.com>
2022-03-06 17:10:45 +0900

    [TIL] 2022 03 06 SUN

--- TDD/habit-tracker/src/tests/presenter.test.js
+++ TDD/habit-tracker/src/tests/presenter.test.js
@@ -14,22 +14,22 @@ describe('All Test of HabitPresenter', () => {
     Presenter = new HabitPresenter(habit);
   });
 
-  test('List length is 3 when Doing anything', () => {
+  it('List length is 3 when Doing anything', () => {
     expect(Presenter.getHabits().length).toBe(3);
   });
 
-  test('List length is 4 when add element', () => {
+  it('List length is 4 when add element', () => {
     Presenter.add('add', update);
     expect(Presenter.getHabits().length).toBe(4);
   });
 
-  test('List length is 2 when remove element', () => {
-    Presenter.delete({ id: 1 }, update);
+  it('List length is 2 when remove element', () => {
+    Presenter.delete(habit[0], update);
     expect(Presenter.getHabits().length).toBe(2);
   });
 
-  test('Target count + 1 when doing Presenter method increment', () => {
-    const target = { id: 1, name: 'Reading', count: 0 };
+  it('Target count + 1 when doing Presenter method increment', () => {
+    const target = habit[0];
     Presenter.increment(target, update);
     const count = Presenter.getHabits().find(
       (item) => item.id === target.id
@@ -37,8 +37,8 @@ describe('All Test of HabitPresenter', () => {
     expect(count).toBe(1);
   });
 
-  test('Doing decrement when count is 0 equal 0', () => {
-    const target = { id: 1, name: 'Reading', count: 0 };
+  it('Doing decrement when count is 0 equal 0', () => {
+    const target = habit[0];
     Presenter.decrement(target, update);
     const count = Presenter.getHabits().find(
       (item) => item.id === target.id
@@ -46,7 +46,7 @@ describe('All Test of HabitPresenter', () => {
     expect(count).toBe(0);
   });
 
-  test('Doing decrement when count is N equal N - 1', () => {
+  it('Doing decrement when count is N equal N - 1', () => {
     const target = { id: 1, name: 'Reading', count: 3 };
     Presenter.decrement(target, update);
     const count = Presenter.getHabits().find(
@@ -55,7 +55,7 @@ describe('All Test of HabitPresenter', () => {
     expect(count).toBe(2);
   });
 
-  test('Doing reset is all list count make 0', () => {
+  it('Doing reset is all list count make 0', () => {
     const mockListData = [
       { id: 1, name: 'Reading', count: 3 },
       { id: 2, name: 'Running', count: 2 },
@@ -68,7 +68,7 @@ describe('All Test of HabitPresenter', () => {
     });
   });
 
-  test('Doing reset when all list count is 0', () => {
+  it('Doing reset when all list count is 0', () => {
     Presenter.reset(update);
     Presenter.getHabits().forEach((item) => {
       expect(item.count).toBe(0);
